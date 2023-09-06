02c922ab143e58d7a0610e32276823503c3607be
orafaelcarvalho <rsouzadecarvalho@gmail.com>
2023-09-05 23:13:27 -0300

    fixing validations

--- CarrosAPI/Models/Carro.cs
+++ CarrosAPI/Models/Carro.cs
@@ -10,10 +10,10 @@ namespace CarrosAPI.Models
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
         [StringLength(45, ErrorMessage = "O campo descrição deve ter no máximo 45 caracteres")]
         public string Descricao { get; set; }
-        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        [Required(ErrorMessage = "O campo marca é obrigatório")]
         [StringLength(45, ErrorMessage = "O campo marca deve ter no máximo 45 caracteres")]
         public string Marca { get; set;}
-        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        [Required(ErrorMessage = "O campo valor é obrigatório")]
         public double Valor { get; set; }
     }
 }
